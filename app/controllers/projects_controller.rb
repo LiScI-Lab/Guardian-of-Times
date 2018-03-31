c71ccfe4da15deb6093e22fb2b73b212e8d875e3
N. Justus <nicola.justus@mni.thm.de>
2018-03-31 18:46:51 +0200

    List all projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,8 @@
 class ProjectsController < ApplicationController
   load_and_authorize_resource
   def index
+    #TODO: use projects from db !
+    @projects = Array.new(5,Project.new(id: -1, name: "test proj", description: "an awesome testproj"))
   end
   def show
   end
