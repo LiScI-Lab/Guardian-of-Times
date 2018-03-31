9d768ae2951aa40d5055258bdef81751201f3a00
Marcel Hoppe <marcel.hoppe@mni.thm.de>
2018-03-31 17:47:25 +0200

    add ability to create projects

--- app/models/ability_project.rb
+++ app/models/ability_project.rb
@@ -2,6 +2,8 @@ module AbilityProject
   include CanCan::Ability
 
   def initialize_project(user, member)
+    can [:create], Project
+
     if member
       can [:show], Project, members: {id: member.id}
     end
