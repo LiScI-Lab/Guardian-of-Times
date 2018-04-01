2bff7b0a6b5d1784f347fdb3731afca60b7d8b44
N. Justus <nicola.justus@mni.thm.de>
2018-04-01 12:41:29 +0200

    Fix typo in 'affected'

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
   has_many :members, class_name: "Project::Member"
   has_many :users, through: :project_members, class_name: "User"
 
-  has_many :tag_affecteds, as: :affecred, class_name: "TagAffected"
+  has_many :tag_affecteds, as: :affected, class_name: "TagAffected"
   has_many :tags, through: :tag_affecteds, class_name: "Tag"
 
   validates :name, presence: true, length: { minimum: Settings.project.name.length_minimum }
--- app/models/project/progress.rb
+++ app/models/project/progress.rb
@@ -1,7 +1,7 @@
 class Project::Progress < ApplicationRecord
   has_and_belongs_to_many :project_members, join_table: :project_progress_participants, class_name: "Project::Member"
 
-  has_many :tag_affecteds, as: :affecred, class_name: "TagAffected"
+  has_many :tag_affecteds, as: :affected, class_name: "TagAffected"
   has_many :tags, through: :tag_affecteds, class_name: "Tag"
 
   validates :start, presence: true
