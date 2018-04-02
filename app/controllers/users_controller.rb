389ec29760b53a0f7ac341686cb3a7e5e35513d3
N. Justus <nicola.justus@mni.thm.de>
2018-04-02 10:05:11 +0200

    Enable editing birthdate of a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,24 @@ class UsersController < ApplicationController
   load_and_authorize_resource
 
   def show
+  end
 
+  def edit
+  end
+
+  def update
+    # render plain:  params.inspect
+    @current_user.attributes = user_params
+    if @current_user.save
+      flash[:success] = "User successfully updated"
+      redirect_to @current_user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:department, :birthdate)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
   resources :projects do
     collection do
       scope :role do
