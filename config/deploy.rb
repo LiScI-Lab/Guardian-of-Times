40b74e1d1440dac73ccc3801bce6153ece6e8607
N. Justus <nicola.justus@mni.thm.de>
2018-05-10 14:02:16 +0200

    user system-wide rbenv in deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.10.1'
 
 
 #setup rbenv as in https://github.com/capistrano/rbenv
-set :rbenv_type, :user
+set :rbenv_type, :system
 set :rbenv_ruby, File.read('.ruby-version').strip
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
