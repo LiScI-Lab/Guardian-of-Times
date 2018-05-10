2c1f35be0344564b4b28b64b20bebb74bcd746d4
Marcel Hoppe <marcel.hoppe@mni.thm.de>
2018-05-11 01:36:39 +0200

    only kept users on welcome. little bit more spacing between welcome and headline

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   authorize_resource class: false
 
   def index
-    @users_count = User.count
+    @users_count = User.kept.count
     @teams_count = Team.kept.count
     @total_time = Team::Progress.kept.pluck(:start,:end).map { |(start,endt)| endt - start }.sum
   end
