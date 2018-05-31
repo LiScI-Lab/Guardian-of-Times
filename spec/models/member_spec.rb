0fe03490beccbe56e7cbeabf59b4c452d4e4b32e
N. Justus <nicola.justus@mni.thm.de>
2018-05-31 15:00:44 +0200

    add specs for target_hours & extra_hours

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -49,4 +49,34 @@ RSpec.describe Team::Member, type: :model do
       expect(member.available?).to be_falsey
     end
   end
+
+  context "with target_hours" do
+    before(:example) do
+      @member = create(:member)
+      @member.target_hours << [Team::Member::TargetHour.new(since: Date.today-1.month, hours: 20), Team::Member::TargetHour.new(since: Date.today-3.month, hours: 40)]
+      @member.save!
+    end
+
+    it "should have target_hours, sorted by `since` date" do
+      expect(@member.target_hours.length).to eq(2)
+      expect(@member.target_hours.last.since).to eq(Date.today-1.month)
+      expect(@member.target_hours.last.hours).to eq(20)
+    end
+    it "should return the matching target_hours for a date" do
+      expect(@member.matching_target_hours(Date.today)).to eq(20)
+      expect(@member.matching_target_hours(Date.today-2.month)).to eq(40)
+    end
+    it "should return 0 hours for unkown timespan" do
+      expect(@member.matching_target_hours(Date.today-2.years)).to eq(0)
+    end
+    it "should calculate extra hours as difference of progresses-target_hours" do
+      month = DateTime.now.beginning_of_month
+      progresses = (0..20).map { |n| Team::Progress.new(member: @member, team: @member.team, start: month+n.days, end: month+n.days+4.hours) }
+      expected_extra_hours = (progresses.map { |p| p.time_spend }.sum/3600 - 20) #20 = target_hours of `month`
+      @member.progresses << progresses
+      @member.save!
+
+      expect(@member.extra_hours(month)).to eq(expected_extra_hours)
+    end
+  end
 end
