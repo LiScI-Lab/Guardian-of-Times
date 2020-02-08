938f4f225268dcefc5e54a2839ed4eb923200e29
Felix Münscher <MAIL>
2020-02-08 14:43:30 +0100

    Removes puts for debug purposes

--- app/helpers/token_generator_helper.rb
+++ app/helpers/token_generator_helper.rb
@@ -2,7 +2,6 @@ module TokenGeneratorHelper
   @@hmac_secret = Settings.api.jwt_secret
 
   def generate_token_for_user(user)
-    puts @@hmac_secret
     exp = (Time.now + 200.years).to_i
     exp_payload = { user: user.id, exp: exp }
     JWT.encode(exp_payload, @@hmac_secret, 'HS256')
@@ -10,7 +9,6 @@ module TokenGeneratorHelper
 
   def verify_token(token)
     decoded_token = JWT.decode(token, @@hmac_secret)
-    puts decoded_token.inspect
     payload, header = decoded_token
     payload["user"]
   end
