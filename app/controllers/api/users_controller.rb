0390196f1077a139d56e0c534a6c4fc30083d8b1
Felix Münscher <Felix.Muenscher@mni.thm.de>
2020-02-26 16:05:34 +0100

    Adds user update and delete. Still needs to be tested though

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,11 +10,30 @@ class Api::UsersController < Api::SecuredApiController
   end
 
   def update
-    #TODO: Implement
+    #TODO: Test!
+    if @current_user.update user_params
+      @message = "User successfully updated"
+      @status = 200
+    else
+      @message = "Something went wrong"
+      @status = 500
+    end
+    render json: @message.to_json, status: @status
   end
 
   def destroy
-    #TODO: Implement
+    #TODO: Test!
+    unless Settings.user.discard.disabled
+      sign_out @user
+      if @user.discard
+        @message = "User successfully discarded"
+        @status = 200
+      else
+        @message = "Something went wrong"
+        @status = 500
+      end
+      render json: @message.to_json, status: @status
+    end
   end
 
   # Plural Routes
@@ -27,5 +46,13 @@ class Api::UsersController < Api::SecuredApiController
     if can? :show, @user
       render json: @user.to_json
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:department, :birth_date,
+                                 :last_name, :first_name, :email,
+                                 :avatar, :avatar_cache, :avatar_type)
   end
 end
