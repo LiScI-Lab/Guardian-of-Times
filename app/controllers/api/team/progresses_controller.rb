736c8685b203a2bae7bf399281c0709ded026954
Felix Münscher <Felix.Muenscher@mni.thm.de>
2020-02-27 15:27:08 +0100

    Adds progresses_controller for Api

---
+++ app/controllers/api/team/progresses_controller.rb
@@ -0,0 +1,11 @@
+class Api::Team::ProgressesController
+  include ::ProgressFilter
+
+  load_and_authorize_resource :team
+  load_and_authorize_resource :progress, through: :team
+
+  def index
+    @progresses = get_filtered_progresses(@team)
+    render json: @progresses
+  end
+end
